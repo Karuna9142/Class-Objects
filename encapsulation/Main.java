5084fcc39b90adc21071bea4442ed3a8931c7cc9
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-07-25 23:34:22 +0530

    Add files via upload

---
+++ encapsulation/Main.java
@@ -0,0 +1,24 @@
+package encapsulation;
+
+class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        System.out.println("Radius of the Circle:- " +circle.getRadius());
+        circle.setRadius(7.0);
+        System.out.println("New radius of the circle:- "+circle.getRadius());
+    }
+}
